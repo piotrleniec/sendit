4f6d6ce371fcf8115f735df795db45795572e147
dimon-k <dmitriy.kanunnikov@gmail.com>
2017-07-22 17:30:01 +0200

    Fix rubocop errors

--- app/controllers/home_page_controller.rb
+++ app/controllers/home_page_controller.rb
@@ -2,9 +2,8 @@ class HomePageController < ApplicationController
   before_action :authenticate_user!
 
   def index
-    if current_user.provider == "facebook"
-      @facebook_data = current_user.facebook(current_user.access_token).get_object("me?fields=name,email,friends")
-    end
+    return unless current_user.provider == 'facebook'
+    @facebook_data = current_user.facebook(current_user.access_token).get_object('me?fields=name,email,friends')
   end
 
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -5,18 +5,18 @@ class User < ApplicationRecord
 
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable,
-         :omniauthable, :omniauth_providers => [:facebook]
+         :omniauthable, omniauth_providers: [:facebook]
 
   def self.from_omniauth(auth)
     where(provider: auth.provider, uid: auth.uid).first_or_create do |user|
       user.email = auth.info.email
-      user.password = Devise.friendly_token[0,20]
-      user.name = auth.info.name   # assuming the user model has a name
-      user.image = auth.info.image # assuming the user model has an image
+      user.password = Devise.friendly_token[0, 20]
+      user.name = auth.info.name
+      user.image = auth.info.image
       user.access_token = auth.credentials.token
       user.access_token_expires_at = Time.at(auth.credentials.expires_at)
-      # If you are using confirmable and the provider(s) you use validate emails,
-      # uncomment the line below to skip the confirmation emails.
+      # If you are using confirmable and the provider(s) you use validate
+      # emails, uncomment the line below to skip the confirmation emails.
       # user.skip_confirmation!
     end
   end
